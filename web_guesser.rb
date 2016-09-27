a8c7602a5eb1d9f028e450f5bf59ec60d6e694a4
Jon Kimble <jonathanbkimble@gmail.com>
2016-09-26 22:13:27 -0600

    added counter for number of guesses

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,40 +1,58 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-SECRET_NUMBER = rand(100)
-
-def check_guess(guess)
-  if (guess.to_i > 100) && (guess.to_i < 104)
-    "Too high!"
-  elsif guess.to_i >= 104
-    "Way too high!"
-  elsif (guess.to_i < -4)
-    "Way too low!"
-  elsif (guess.to_i < 0) && (guess.to_i >= -4)
-    "Too low!"
-  elsif guess.to_i == SECRET_NUMBER
-    "You got it right!"
-    "The SECRET NUMBER is #{SECRET_NUMBER}"
+  def generate_secret_number
+    @secret_number = rand(100)
+    @@count = 6
   end
-end
 
-def choose_background_color(guess)
-  if (guess.to_i > 100) && (guess.to_i < 104)
-    "Pink"
-  elsif guess.to_i >= 104
-    "Red"
-  elsif (guess.to_i < -4)
-    "Red"
-  elsif (guess.to_i < 0) && (guess.to_i >= -4)
-    "Pink"
-  elsif guess.to_i == SECRET_NUMBER
-    "Green"
+  def check_guess(guess)
+    if (guess.to_i > 100) && (guess.to_i < 104)
+      "Too high!"
+    elsif guess.to_i >= 104
+      "Way too high!"
+    elsif (guess.to_i < -4)
+      "Way too low!"
+    elsif (guess.to_i < 0) && (guess.to_i >= -4)
+      "Too low!"
+    elsif guess.to_i == @secret_number
+      "You got it right!"
+      "The SECRET NUMBER is #{@secret_number}"
+    end
   end
-end
 
-get '/' do
-  guess = params["guess"]
-  message = check_guess(guess)
-  color_choice = choose_background_color(guess)
-  erb :index, :locals => {:message => message, :color_choice => color_choice}
-end
+  def choose_background_color(guess)
+    if (guess.to_i > 100) && (guess.to_i < 104)
+      "Pink"
+    elsif guess.to_i >= 104
+      "Red"
+    elsif (guess.to_i < -4)
+      "Red"
+    elsif (guess.to_i < 0) && (guess.to_i >= -4)
+      "Pink"
+    elsif guess.to_i == @secret_number
+      "Green"
+    end
+  end
+
+  def keeps_count(guess)
+    if @@count < 1
+      generate_secret_number
+      return "You have lost, a new secret number has been generated"
+    end
+    
+    if (guess.to_i > -1) && (guess.to_i < 101) && (guess != "")
+      @@count -= 1
+      return "You have #{@@count} guesses left . . ."
+    end
+  end
+
+  generate_secret_number
+
+  get '/' do
+    guess = params["guess"]
+    message = check_guess(guess)
+    color_choice = choose_background_color(guess)
+    display_count = keeps_count(guess)
+    erb :index, :locals => {:message => message, :color_choice => color_choice, :display_count => display_count}
+  end

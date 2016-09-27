69d22d2ed1f3d50f5724982f55d7e000d156e51b
Jon Kimble <jonathanbkimble@gmail.com>
2016-09-26 21:43:39 -0600

    added user feedback and color changing

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,40 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-number = rand(100)
+SECRET_NUMBER = rand(100)
+
+def check_guess(guess)
+  if (guess.to_i > 100) && (guess.to_i < 104)
+    "Too high!"
+  elsif guess.to_i >= 104
+    "Way too high!"
+  elsif (guess.to_i < -4)
+    "Way too low!"
+  elsif (guess.to_i < 0) && (guess.to_i >= -4)
+    "Too low!"
+  elsif guess.to_i == SECRET_NUMBER
+    "You got it right!"
+    "The SECRET NUMBER is #{SECRET_NUMBER}"
+  end
+end
+
+def choose_background_color(guess)
+  if (guess.to_i > 100) && (guess.to_i < 104)
+    "Pink"
+  elsif guess.to_i >= 104
+    "Red"
+  elsif (guess.to_i < -4)
+    "Red"
+  elsif (guess.to_i < 0) && (guess.to_i >= -4)
+    "Pink"
+  elsif guess.to_i == SECRET_NUMBER
+    "Green"
+  end
+end
 
 get '/' do
-  erb :index, :locals => {:number => number}
+  guess = params["guess"]
+  message = check_guess(guess)
+  color_choice = choose_background_color(guess)
+  erb :index, :locals => {:message => message, :color_choice => color_choice}
 end
